b3792420f2455584d4b08d7e9c75230b236fdeb1
Vadim Zaretskiy <vadizar@mobidevices.ru>
2017-07-07 18:26:32 +0300

    Prevent links in standalone web apps opening Mobile Safari

---
+++ stay_standalone.js
@@ -0,0 +1,10 @@
+// Mobile Safari in standalone mode
+if(window.navigator.standalone){
+    var a=document.getElementsByTagName("a");
+    for(var i=0;i<a.length;i++) {
+        a[i].onclick=function() {
+            window.location=this.getAttribute("href");
+            return false
+        }
+    }
+}
